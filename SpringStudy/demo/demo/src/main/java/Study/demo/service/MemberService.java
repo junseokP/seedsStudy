1e4468a3f71bd1d0b335a31195f1efa533e84d55
HOIVVVV <152933551+HOIVVVV@users.noreply.github.com>
2024-04-10 13:37:50 +0900

    Add files via upload

---
+++ SpringStudy/demo/demo/src/main/java/Study/demo/service/MemberService.java
@@ -0,0 +1,33 @@
+package Study.demo.service;
+
+import Study.demo.domain.Member;
+import Study.demo.repository.MemberRepository;
+import Study.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
